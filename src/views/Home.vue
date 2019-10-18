908ba465cd234519e4ebd514bffd962bf923c9f9
pdeng <635639654@qq.com>
2019-10-18 10:21:28 +0800

    修改首页样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,26 @@
+<!--
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-10-17 09:59:49
+ * @LastEditTime: 2019-10-18 10:20:50
+ * @LastEditors: Please set LastEditors
+ -->
+
 <template>
   <div class="home">
-    <h1 style="text-align:center">X · Charts</h1>
+    <div class="box-container">X · Charts</div>
   </div>
 </template>
+<style lang="scss">
+.box-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-weight: 700;
+  font-size: 50px;
+}
+</style>
 <script>
 export default {
   name: 'Home'
