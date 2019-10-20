0a1f79ff49d1ebe844809142e8cadfb73f74e1c0
pdeng <635639654@qq.com>
2019-10-20 22:57:31 +0800

    新增 map 地图类型

---
+++ src/utils/xcharts/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: x-charts 模块暴露接口
+ * @Author: pdeng
+ * @Date: 2019-10-16 19:54:06
+ * @LastEditTime: 2019-10-17 10:10:36
+ * @LastEditors: Please set LastEditors
+ */
+import Xcharts from './core'
+export { Xcharts }
