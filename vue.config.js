df3076e878dbcef1e226378804f5c76e97e97e94
pdeng <635639654@qq.com>
2019-10-20 22:56:22 +0800

    添加 map 类型地图数据

--- vue.config.js
+++ vue.config.js
@@ -13,5 +13,33 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: process.env.NODE_ENV === 'production' ? '/x-charts/dist' : ''
+  publicPath: process.env.NODE_ENV === 'production' ? '/x-charts/dist' : '',
+  devServer: {
+    host: '0.0.0.0',
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    proxy: {
+      // change xxx-api/login => mock/login
+      // detail: https://cli.vuejs.org/config/#devserver-proxy
+      [process.env.VUE_APP_BASE_API]: {
+        target: `http://0.0.0.0:${port}/mock`,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      },
+      '/dev-api': {
+        target: `http://0.0.0.0:${port}/mock`,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/dev-api': ''
+        }
+      }
+    }
+    // after: require('./mock/mock-server.js')
+  }
 }
